7a40e682c18cb5c15f49bbe678ec4c670b594876
Fanni Rezsek <109975402+Cola39@users.noreply.github.com>
2026-06-23 14:54:14 +0200

    Refactor shopping.js for improved readability and structure

--- shopping.js
+++ shopping.js
@@ -1,99 +1,95 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxNCN0v54YmAPkFwv_Ie892IsvF80uAHt25lV56SYq4nvfEgvJLRnTANShnuIqYvgqvog/exec';
-
-const fetchURL = scriptURL + "?type=shopping";
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxNCN0v54YmAPkFwv_Ie892IsvF80uAHt25lV56SYq4nvfEgvJLRnTANShnuIqYvgqvog/exec';
+const FETCH_URL = `${SCRIPT_URL}?type=shopping`;
 
 let shoppingData = [];
 
-window.onload = function() {
-  fetch(fetchURL)
-    .then(response => response.json())
+window.onload = () => {
+  fetch(FETCH_URL)
+    .then(res => res.json())
     .then(data => {
       shoppingData = data.map(row => ({
-        taska: row[0] || "",
-        kat: row[1] || "",
+        taska: row[0] || "hűtőtáska",
+        kat: row[1] || "1",
         targy: row[2] || "",
-        db: row[3] || "",
-        kosarban: row[4] === true || row[4] === "TRUE"
+        db: row[3] || "1",
+        kosarban: String(row[4]).toUpperCase() === "TRUE"
       }));
       
-      shoppingData.sort((a, b) => a.kat - b.kat);
-      
+      sortAndRender();
       document.getElementById('loading').style.display = 'none';
       document.getElementById('shoppingTable').style.display = 'table';
-      renderTable();
-    });
+    })
+    .catch(err => console.error('Fetch error:', err));
 };
+
+function sortAndRender() {
+  shoppingData.sort((a, b) => a.kat - b.kat);
+  renderTable();
+}
 
 function renderTable() {
   const tbody = document.getElementById('tableBody');
   tbody.innerHTML = "";
 
   shoppingData.forEach((item, index) => {
-    const isDoneClass = item.kosarban ? "done-row" : "";
+    const row = document.createElement('tr');
+    if (item.kosarban) row.className = "done-row";
     
-    tbody.innerHTML += `
-      <tr class="${isDoneClass}">
-        <td>
-          <select onchange="updateData(${index}, 'taska', this.value)">
-            <option value="hűtőtáska" ${item.taska === 'hűtőtáska' ? 'selected' : ''}>hűtőtáska</option>
-            <option value="vászontáska" ${item.taska === 'vászontáska' ? 'selected' : ''}>vászontáska</option>
-          </select>
-        </td>
-        <td><input type="number" value="${item.kat}" oninput="updateData(${index}, 'kat', this.value)"></td>
-        <td><input type="text" value="${item.targy}" oninput="updateData(${index}, 'targy', this.value)"></td>
-        <td><input type="number" value="${item.db}" oninput="updateData(${index}, 'db', this.value)"></td>
-        <td style="text-align:center;">
-          <input type="checkbox" ${item.kosarban ? 'checked' : ''} onchange="toggleCheckbox(${index}, this.checked)">
-        </td>
-        <td><button class="delete-btn" onclick="deleteRow(${index})">🗑️</button></td>
-      </tr>
+    row.innerHTML = `
+      <td>
+        <select onchange="updateData(${index}, 'taska', this.value)">
+          <option value="hűtőtáska" ${item.taska === 'hűtőtáska' ? 'selected' : ''}>hűtőtáska</option>
+          <option value="vászontáska" ${item.taska === 'vászontáska' ? 'selected' : ''}>vászontáska</option>
+        </select>
+      </td>
+      <td><input type="number" value="${item.kat}" oninput="updateData(${index}, 'kat', this.value)"></td>
+      <td><input type="text" value="${item.targy}" oninput="updateData(${index}, 'targy', this.value)"></td>
+      <td><input type="number" value="${item.db}" oninput="updateData(${index}, 'db', this.value)"></td>
+      <td style="text-align:center;">
+        <input type="checkbox" ${item.kosarban ? 'checked' : ''} onchange="toggleCheckbox(${index}, this.checked)">
+      </td>
+      <td><button class="delete-btn" onclick="deleteRow(${index})">🗑️</button></td>
     `;
+    tbody.appendChild(row);
   });
 }
 
-function updateData(index, field, value) {
-  shoppingData[index][field] = value;
-}
+function updateData(i, field, val) { shoppingData[i][field] = val; }
 
-function toggleCheckbox(index, isChecked) {
-  shoppingData[index].kosarban = isChecked;
-  renderTable(); 
-}
-
-function addRow() {
-  shoppingData.push({ taska: "hűtőtáska", kat: "1", targy: "", db: "1", kosarban: false });
+function toggleCheckbox(i, isChecked) {
+  shoppingData[i].kosarban = isChecked;
   renderTable();
 }
 
-function deleteRow(index) {
-  shoppingData.splice(index, 1);
+function addRow() {
+  shoppingData.push({ taska: "hűtőtáska", kat: "9", targy: "", db: "1", kosarban: false });
+  sortAndRender();
+}
+
+function deleteRow(i) {
+  shoppingData.splice(i, 1);
   renderTable();
 }
 
 function saveShoppingList() {
-  const button = document.querySelector('.btn');
-  const originalText = button.innerText;
-  button.innerText = 'Mentés...';
+  const btn = document.querySelector('.btn');
+  const originalText = btn.innerText;
+  btn.innerText = 'Mentés...';
+  btn.disabled = true;
 
   const formData = new URLSearchParams();
-  formData.append("type", "shopping"); 
-  formData.append("data", JSON.stringify(shoppingData)); 
+  formData.append("type", "shopping");
+  formData.append("data", JSON.stringify(shoppingData));
 
-  fetch(scriptURL, {
-    method: 'POST',
-    body: formData 
-  })
-  .then(response => {
-    if (!response.ok) throw new Error('Network error');
-    alert('Lista sikeresen frissítve!');
-    button.innerText = originalText;
-    
-    shoppingData.sort((a, b) => a.kat - b.kat);
-    renderTable();
-  })
-  .catch(error => {
-    console.error('Error!', error);
-    alert('Hiba történt a mentés során.');
-    button.innerText = originalText;
-  });
+  fetch(SCRIPT_URL, { method: 'POST', body: formData })
+    .then(res => {
+      if (!res.ok) throw new Error();
+      alert('Sikeres mentés!');
+      sortAndRender();
+    })
+    .catch(() => alert('Hiba történt!'))
+    .finally(() => {
+      btn.innerText = originalText;
+      btn.disabled = false;
+    });
 }
